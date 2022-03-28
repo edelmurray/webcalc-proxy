975d9117c0f7b6bebbbddc6a85e1fd40a2d5d8d8
edelmurray <99981772+edelmurray@users.noreply.github.com>
2022-03-28 15:42:16 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+from typing import Dict, List
+
+urls: dict[str, list[str]] = {
+    "add/": ["http://add.webcalc.40146762.qpc.hal.davecutting.uk"],
+    "subtract/": ["http://subtract.webcalc.40146762.qpc.hal.davecutting.uk"],
+    "multiply/": ["http://multiply.webcalc.40146762.qpc.hal.davecutting.uk"],
+    "exponent/": ["http://exponent.webcalc.40146762.qpc.hal.davecutting.uk"],
+    "divide/": ["http://divide.webcalc.40146762.qpc.hal.davecutting.uk"],
+    "modulus/": ["https://us-central1-webcalc-modulus.cloudfunctions.net/calculate"]
+}
+
+origin = ["http://calc.40146762.qpc.hal.davecutting.uk"]

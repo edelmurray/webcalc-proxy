0901a8ec555cc0fecf41b6201265cd108e22ce30
edelmurray <99981772+edelmurray@users.noreply.github.com>
2022-04-07 14:59:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,6 @@ def getKeysByValue(urldict, valuetofind):
             return newurl
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
     # app.run()
